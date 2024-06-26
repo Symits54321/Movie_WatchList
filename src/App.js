e555d587566d9a5273ed50cb3f65a22daa6e2258
Symits54321 <125802160+Symits54321@users.noreply.github.com>
2024-06-26 09:44:11 +0530

    installed necessary libraries

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import {Provider} from "react-redux";
+import {store} from "./store";
+
+import Chatpage from './pages/chatpage/Chatpage';
+import Loginpage from './pages/loginpage/Loginpage';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
 
 function App() {
+   
+
+  const browserRouter = createBrowserRouter([{
+
+    path:'/',
+    element:<Root/>,
+    children:[
+      {path:"login", element:<Loginpage/>,},
+      {path:"chatpage", element:<Chatpage/>,},
+    ],
+  },]);
+
+
+
+
+
   return (
-    <div className="App">
-    
+    <Provider store={store}>
+      <div className="App">
+        {/* it is the main page for all chat execution  */}
+        <RouterProvider  router={browserRouter} />
+      </div>
+    </Provider>
+  );
+}
+
+
+function Root() {
+  return (
+    <div>
+      <h1 style={{margin:"30px 10px"}}>Welcome to the Chat App</h1>
+      <Outlet />
     </div>
   );
 }
