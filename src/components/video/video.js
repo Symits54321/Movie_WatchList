9164158ec98974f16318b16a7652a305552eba4a
Symits54321 <125802160+Symits54321@users.noreply.github.com>
2024-06-28 14:25:28 +0530

    fix: youtube video bug

--- src/components/video/video.js
+++ src/components/video/video.js
@@ -1,26 +1,50 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { movieActions,movieSelector } from '../../redux/reducers/movieReducer';
 
+import YouTube from 'react-youtube';
 
 function VideoComp () {
 
     let {currMovieData} = useSelector(movieSelector) ;
 
     let {title,videoUrl}=currMovieData;
+    
+
+    // Youtube 
+
+    const getYouTubeVideoIdSubstring = (url) => {
+        if (typeof url !== 'string') {
+            return null; // Return null or handle error as appropriate
+        }
+    
+        let videoId = null;
+        const startIndex = url.indexOf('v=');
+        if (startIndex !== -1) {
+            const endIndex = url.indexOf('&', startIndex);
+            videoId = endIndex === -1 ? url.substring(startIndex + 2) : url.substring(startIndex + 2, endIndex);
+        }
+        return videoId;
+    };
+
+    const videoId = getYouTubeVideoIdSubstring(videoUrl);
+
+    const opts = {
+        height: '390',
+        width: '640',
+        playerVars: {
+            autoplay: 1,
+        },
+    };
+
+
+
+
 
     return(
 
         <div>
-        {videoUrl!="" ? (
-            <iframe
-                width="560"
-                height="315"
-                src={videoUrl}
-                title={title}
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-            ></iframe>
+        {videoId!="" ? (
+            <YouTube videoId={videoId} opts={opts} />
         ) : (
             <p>No video available</p>
         )}
